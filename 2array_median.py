ff07837fc98b7e21420a6b4f9c615270b1cee5d5
Sachin Singh <sachin.singh239269@gmail.com>
2025-01-20 22:58:39 +0530

    Complete code ...

---
+++ 2array_median.py
@@ -0,0 +1,14 @@
+
+import numpy as np 
+from numpy import *
+
+arr1 = np.array( [1,2], int)
+
+arr2 = np.array([3,4], int)
+
+merged_arr = arr1 + arr2
+merged_arr1 = median(merged_arr)/2
+
+
+print('THe Median of array {} and array2 {} is  = {}'.format(arr1,arr2,merged_arr1))  
+
